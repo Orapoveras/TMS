63dbe52849e519bdc451c28635276737772f1099
Ihar Piatrenka <piatrenka@gmail.com>
2016-01-27 14:30:43 +0300

    First sketch of app navigation bar

--- 05_source/DB/PDM/altbas_1601051245.sql
+++ 05_source/DB/PDM/altbas_1601051245.sql
@@ -1,62 +1,7 @@
-/*==============================================================*/
+﻿/*==============================================================*/
 /* DBMS name:      ORACLE Version 11g                           */
 /* Created on:     05/01/2016 12:45:37                          */
 /*==============================================================*/
-
-
-
--- Type package declaration
-create or replace package PDTypes  
-as
-    TYPE ref_cursor IS REF CURSOR;
-end;
-
--- Integrity package declaration
-create or replace package IntegrityPackage AS
- procedure InitNestLevel;
- function GetNestLevel return number;
- procedure NextNestLevel;
- procedure PreviousNestLevel;
- end IntegrityPackage;
-/
-
--- Integrity package definition
-create or replace package body IntegrityPackage AS
- NestLevel number;
-
--- Procedure to initialize the trigger nest level
- procedure InitNestLevel is
- begin
- NestLevel := 0;
- end;
-
-
--- Function to return the trigger nest level
- function GetNestLevel return number is
- begin
- if NestLevel is null then
-     NestLevel := 0;
- end if;
- return(NestLevel);
- end;
-
--- Procedure to increase the trigger nest level
- procedure NextNestLevel is
- begin
- if NestLevel is null then
-     NestLevel := 0;
- end if;
- NestLevel := NestLevel + 1;
- end;
-
--- Procedure to decrease the trigger nest level
- procedure PreviousNestLevel is
- begin
- NestLevel := NestLevel - 1;
- end;
-
- end IntegrityPackage;
-/
 
 
 drop trigger tAssignements_bi_x
